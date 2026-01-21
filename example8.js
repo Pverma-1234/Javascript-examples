73858d0b5e57152958b2e107b03e47341c6227a0
princeverma <princeverma2072644@gmail.com>
2026-01-21 23:05:34 +0530

    Advance Comperative oper

---
+++ example8.js
@@ -0,0 +1,3 @@
+let x;
+let y= x ??=10;//nullish operator(x ki koi value hai ya nhi ye check ho rha hai agar nhi hai koi value toh ten hojegi wrnha jo hogi wahi hojegi)
+console.log(y);
---
+++ example9.js
@@ -0,0 +1,10 @@
+//if else and switch control
+let x=10;
+if(x===10){
+    console.log("Value of x is:",x);
+}else if(x<10){
+    console.log("X is less than 10");
+    }
+else{
+    console.log("The value is not:",x);
+}
