bed47ca0de1167ec1c7f207d2711cca4bf6e0771
princeverma <princeverma2072644@gmail.com>
2026-01-21 22:59:37 +0530

    example4

---
+++ example4.js
@@ -0,0 +1,8 @@
+let a= Number("10");
+let y=10;
+console.log(a);
+console.log(typeof y);
+
+let j=5;
+let k=9;
+console.log(j+k);
---
+++ example5.js
@@ -0,0 +1,8 @@
+//operators
+
+let x=2;
+let y=10;
+
+console.log(x+y);
+x+=4;
+console.log(x);
