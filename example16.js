2029190ae29e6c4908eca6ef30818e27eadf7dbc
princeverma <princeverma2072644@gmail.com>
2026-01-22 10:47:17 +0530

    Understanding Arrow Functions

---
+++ example16.js
@@ -0,0 +1,29 @@
+//Function as a variable
+// const add =function addNumbers(a,b){
+//     return a+b;
+// }
+
+// const r=add(10,20);
+// console.log(r);
+
+
+// Arrow function : another way of using functions
+const add = (a,b) => {
+    a+b;
+}
+
+
+const  sub = (a,b) => {
+    a-b;
+}
+
+const div = (a,b) => {
+    a/b;
+}
+
+const multiply = (a,b) =>{
+    a*b;
+}
+
+const r=add(10,20);
+console.log(r);
