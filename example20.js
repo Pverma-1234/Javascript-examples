eed7cd0fdd27dc54b2a0b4df0722ce9384e23d5f
princeverma <princeverma2072644@gmail.com>
2026-01-23 09:29:20 +0530

    Use of Rest

---
+++ example20.js
@@ -0,0 +1,15 @@
+function sum(...nums){  //whatever is coming take rest of it
+    let sum=0; 
+    let i=0;
+    while(i<nums.length){
+        sum+=nums[i];
+        i++;
+    }
+    return sum;
+}
+
+const r1=sum(10,20,30);
+const r2 = sum(10,20,30,40);
+
+console.log(r1);
+console.log(r2);
